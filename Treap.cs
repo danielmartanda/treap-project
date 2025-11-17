3fb1de793fab62bc055e27783a9dd5c1fe988c79
ussanth <ussanthbalasingam@trentu.ca>
2025-11-16 23:41:58 -0500

    added comments

--- Treap.cs
+++ Treap.cs
@@ -1,5 +1,29 @@
+/* 
+================================================================
+Course: COIS 3020 - Data Structures and Algorithms II
+Assignment 2: Treap Data Structure
+Date: November 2025
 
+Project Description: 
+This program implements a treap data structure, a probabilistic binary search tree
+that combines the properties of a binary search tree and a heap. It allows Insertion, 
+Deletion, Search, Split, Merge & Range Queries operations to be performed. 
 
+Authors:
+1. Ussanth Balasingam - Student ID: 0765174
+2. Daniel Martanda - Student ID: 0813510
+
+================================================================
+*/
+
+/* 
+--------------------------------------------
+Treap Data Structure   
+--------------------------------------------
+- 
+- 
+- 
+*/
 using System;
 using System.Collections.Generic;
 
